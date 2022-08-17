8e1926e32301f60c705553ed631306b1c6ba008c
Zeynep <zeynep.kandil@kafein.com.tr>
2022-08-17 11:50:43 +0300

    Initial Commit

---
+++ MovieBox-MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBox-MVC
+//
+//  Created by Kafein on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
